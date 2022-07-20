9210b2ac0a10f50c6866212130ceb9f89754f926
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-07-20 16:26:46 +0200

    create NormalizedBox

---
+++ include/normalized_box.h
@@ -0,0 +1,21 @@
+#ifndef NORMALIZED_BOX_H
+#define NORMALIZED_BOX_H
+
+#include <opencv2/core.hpp>
+
+class NormalizedBox {
+  float cx;
+  float cy;
+  float w;
+  float h;
+
+ public:
+  NormalizedBox(float cx_, float cy_, float w_, float h_);
+
+  static NormalizedBox fromYolo(float yoloX_, float yoloY_, float yoloW_, float yoloH_, cv::Size size);
+  static NormalizedBox fromRect(cv::Rect rect, cv::Size size);
+
+  cv::Rect toRect(cv::Size size) const;
+};
+
+#endif  // NORMALIZED_BOX_H
---
+++ src/normalized_box.cpp
@@ -0,0 +1,51 @@
+#include "normalized_box.h"
+
+#include <opencv2/core.hpp>
+#include <stdexcept>
+
+NormalizedBox::NormalizedBox(float cx_, float cy_, float w_, float h_) {
+  if (cx_ > 1 || cx_ < 0) {
+    throw std::invalid_argument("Normalized center x must be in range [0, 1]");
+  }
+  if (cy_ > 1 || cy_ < 0) {
+    throw std::invalid_argument("Normalized center y must be in range [0, 1]");
+  }
+  if (w_ > 1 || w_ < 0) {
+    throw std::invalid_argument("Normalized width must be in range [0, 1]");
+  }
+  if (h_ > 1 || h_ < 0) {
+    throw std::invalid_argument("Normalized height must be in range [0, 1]");
+  }
+
+  cx = cx_;
+  cy = cy_;
+  w = w_;
+  h = h_;
+}
+
+NormalizedBox NormalizedBox::fromYolo(float yoloX_, float yoloY_, float yoloW_, float yoloH_, cv::Size size) {
+  float cx_ = yoloX_ / size.width;
+  float cy_ = yoloY_ / size.height;
+  float w_ = yoloW_ / size.width;
+  float h_ = yoloH_ / size.height;
+
+  return NormalizedBox(cx_, cy_, w_, h_);
+}
+
+NormalizedBox NormalizedBox::fromRect(cv::Rect rect, cv::Size size) {
+  float cx_ = (rect.x + (rect.width / 2)) / size.width;
+  float cy_ = (rect.y + (rect.height / 2)) / size.height;
+  float w_ = rect.width / size.width;
+  float h_ = rect.height / size.height;
+
+  return NormalizedBox(cx_, cy_, w_, h_);
+}
+
+cv::Rect NormalizedBox::toRect(cv::Size size) const {
+  float x_ = (cx * size.width) - (w * size.width / 2);
+  float y_ = (cy * size.height) - (h * size.height / 2);
+  float w_ = w * size.width;
+  float h_ = h * size.height;
+
+  return cv::Rect(x_, y_, w_, h_);
+}
